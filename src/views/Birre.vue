44870b27ff866391fd2b44fb5ac3634edced72ea
Alessandro <aleneri242@gmail.com>
2021-05-12 22:29:28 +0200

    aggiornamento

--- src/views/Birre.vue
+++ src/views/Birre.vue
@@ -4,8 +4,8 @@
       <!-- <Title value="Birre" /> -->
       <h2>Pagina {{page}}</h2>
       <div class="btn">
-        <button type="button" name="prevBtn" @click="prevPage(), scrollToTop()"><i class="fas fa-arrow-circle-left"></i></button>
-        <button type="button" name="nextBtn" @click="nextPage(), scrollToTop()"><i class="fas fa-arrow-circle-right"></i></button>
+        <button type="button" name="prevBtn" @click="prevPage(), scrollToTop()"><i class="fas fa-arrow-left"></i></button>
+        <button type="button" name="nextBtn" @click="nextPage(), scrollToTop()"><i class="fas fa-arrow-right"></i></button>
       </div>
 
 
@@ -14,17 +14,17 @@
       <div class="details" v-for="beer in beers" :key="beer.id">
         <router-link to="/dettaglio/:id">
           <div class="info-beer">
-            <h3>{{beer.name}}</h3>
+            <h2>{{beer.name}}</h2>
             <h4>{{beer.tagline}}</h4>
             <p>{{beer.description}}</p>
 
           </div>
           <div class="img-beer">
             <div class="img-beer-top">
-              <img :src="beer.image_url" alt="beer-img">
+              <img v-if="beer.image_url != null" :src="beer.image_url" alt="beer-img">
             </div>
             <div class="img-beer-bottom">
-              <h4>Grado alcolico: {{beer.abv}}%</h4>
+              <h5>Grado alcolico: {{beer.abv}}%</h5>
             </div>
           </div>
         </router-link>
@@ -32,8 +32,8 @@
 
     </div>
     <div class="btn">
-      <button type="button" name="prevBtn" @click="prevPage(), scrollToTop()"><i class="fas fa-arrow-circle-left"></i></button>
-      <button type="button" name="nextBtn" @click="nextPage(), scrollToTop()"><i class="fas fa-arrow-circle-right"></i></button>
+      <button type="button" name="prevBtn" @click="prevPage(), scrollToTop()"><i class="fas fa-arrow-left"></i></button>
+      <button type="button" name="nextBtn" @click="nextPage(), scrollToTop()"><i class="fas fa-arrow-right"></i></button>
     </div>
     <footer></footer>
 
@@ -95,6 +95,12 @@ button {
   border-radius: 100%;
   margin: 5px;
   background-color: #FFD23F;
+  cursor: pointer;
+}
+
+i {
+  color: white;
+  font-size: 30px;
 }
 
 
@@ -102,7 +108,7 @@ button {
 .container {
   width: 100%;
   /* height: 100vh; */
-  background-color: whitesmoke;
+  background-color: white;
   padding: 50px 100px;
   display: flex;
   justify-content: center;
@@ -111,9 +117,9 @@ button {
 }
 
 .details {
-  background-color: #FFD23F;
+  background-color: whitesmoke;
   width: calc(100% / 2 - 20px);
-  height: 320px;
+  height: 340px;
   margin: 10px;
   overflow: hidden;
   cursor: pointer;
@@ -121,8 +127,13 @@ button {
   border-radius: 25px;
 }
 
-h3, h4 {
-  margin: 5px;
+h4 {
+  margin-bottom: 30px;
+}
+
+h5 {
+  margin-top: 15px;
+  font-size: 18px;
 }
 
 p {
@@ -141,6 +152,7 @@ p {
   width: 35%;
   padding: 20px;
   float: left;
+  border-right: 2px solid #3C2C23;
 }
 
 .img-beer-top {
@@ -148,8 +160,8 @@ p {
 }
 
 .img-beer-bottom {
-    height: 20%;
-    padding: 5px;
+  height: 20%;
+  padding: 5px;
 }
 
 .info-beer {
@@ -157,7 +169,8 @@ p {
   width: 65%;
   float: right;
   padding: 5px;
-  background-color: white;
+  background-image: url('https://f.vividscreen.info/soft/60f60e41ad82985d44a51f9cd4109c4e/Beer-Texture-1920x1080.jpg');
+  background-size: cover;
 }
 
 img {
